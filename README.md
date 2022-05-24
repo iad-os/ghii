f15c4c68ba9f9d5ad05cf9e6b7863fb5a5574611
angelita.doria <angelita.doria@iad2.it>
2022-05-24 18:39:59 +0200

    Update readme and add typeDocs

--- README.md
+++ README.md
@@ -1,3 +1,99 @@
 # GHII
 
-TBC
+## WHAT'S GHII?
+When an application has multiple configurations it has to check them but the risk is to lose something. <br />
+Ghii is a library to validate and manage app configurations (environmental variables, json package, http, yaml) in a single object ensuring simplicity and security.
+Ghii gives you permission to access the object for application information obtained by asynchronous methods.
+
+## HOW IS IT WORK?
+First you must create a ghii object and define all app configurations you want validate (like the example: env, name, config):
+```TypeScript
+import ghii from '@ghii/ghii';
+import envsLoader from '@ghii/envs-loader';
+
+const options = ghii<{
+  env: 'development' | 'production';
+  name: {
+    appVersion: string;
+  };
+  configs: {
+    url: { local: string; path: string }[];
+  };
+}>()
+...
+```
+Then you can create sections.
+In every **section** you can indicate a default value and define a **validator** (for validation Joi is used).
+
+```TypeScript
+const options = ghii<{
+  env: 'development' | 'production';
+  name: {
+    appVersion: string;
+  };
+  configs: {
+    url: { local: string; path: string }[];
+  };
+}>()
+  .section('env', {
+    validator: joi => joi.string().allow('development', 'production'),
+    defaults: 'production',
+  })
+  .section('name', {
+    defaults: {
+      appVersion: '0.0.1',
+    },
+    validator: joi =>
+      joi.object({
+        appVersion: joi.string(),
+      }),
+  })
+  ...
+```
+
+Finally you can define a **loader**, an asynchronous function withouth arguments, that return a generic object.
+In this object you can define some or all part of your app configurations (in the example load configurations via environment variables with MYAPP prefix):
+```TypeScript
+.loader(envsLoader({ envs: process.env, prefix: 'MYAPP' }));
+```
+In the index page you must define the entry point for ghii (options is the import for ghii configuration) and at this point you can access on all ghii informations sistematically (in the example you obtain informations about the last app snapshot):
+```TypeScript
+import './util/configs';
+import options from './config/options';
+import log from './config/log';
+
+(async () => {
+  const logger = log({
+    xRequest: 'ghii',
+    tags: ['ghii-snapshot'],
+  });
+
+  try {
+    await options.waitForFirstSnapshot({ timeout: 10000 }, __dirname, './main');
+    logger.debug({ options: options.snapshot() }, 'CONFIG-SNAPSHOT - OK');
+  } catch (err) {
+    logger.error(err, 'CONFIG-SNAPSHOT - KO');
+  }
+})();
+```
+When starting the app, ghii start, called all validators and then invokes all loaders, create an unique object to validate configurations. If all is ok the app starts up, otherwise it stops.
+### Ghii Loader Repository
+Here there are some loader you can install into your application:
+* YAML LOADER: https://github.com/iad-os/ghii-yaml-loader
+* HTTP LOADER: https://github.com/iad-os/ghii-http-loader
+* PACKAGE-JSON LOADER: https://github.com/iad-os/ghii-package-json-loader
+* ENVS LOADER: https://github.com/iad-os/ghii-envs-loader
+### USE AND SETUP
+
+```sh
+npm install @ghii/ghii
+npm install @ghii/yaml-loader
+npm install @ghii/http-loader
+npm install @ghii/package-json-loader
+npm install @ghii/envs-loader
+```
+### STARTER
+* https://gitlab.iad2.it/cloud-coach/starters/ts-mongo-express-starter
+### EXAMPLES
+* https://github.com/iad-os/ghii-nodejs-typescrypt-express-example
+
--- src/ghii.ts
+++ src/ghii.ts
@@ -4,13 +4,20 @@ import { cloneDeep, map, merge, reduce } from 'lodash';
 import path from 'path';
 import { PartialDeep, ValueOf } from 'type-fest';
 import TypedEventEmitter from './TypedEventEmitter';
+
+/** @interface Topic
+ * @param T
+ * @property {PartialDeep<T> | undefined} defaults
+ * @method {Joi.Schema} validator {@param {typeof Joi} joi}
+ */
 export interface Topic<T> {
   defaults?: PartialDeep<T> | T;
   validator: (joi: typeof Joi) => Joi.Schema;
 }
-
+/**@type Loader */
 export type Loader = () => Promise<{ [key: string]: unknown }>;
 
+/** @type GhiiInstance */
 export type GhiiInstance<O extends { [P in keyof O]: O[P] }> = {
   section: <K extends keyof O>(this: GhiiInstance<O>, name: K, topic: Topic<O[K]>) => GhiiInstance<O>;
   loader: (this: GhiiInstance<O>, loader: Loader) => GhiiInstance<O>;
@@ -25,18 +32,30 @@ export type GhiiInstance<O extends { [P in keyof O]: O[P] }> = {
   on: ValueOf<Pick<GhiiEmitter<EventTypes<O>>, 'on'>>;
   once: ValueOf<Pick<GhiiEmitter<EventTypes<O>>, 'once'>>;
 };
+/**@type Snapshot */
 export type Snapshot<O extends { [P in keyof O]: O[P] }> = { [P in keyof O]: O[P] };
+/**@type Sections */
 export type Sections<O extends { [P in keyof O]: O[P] }> = { [key in keyof O]?: Topic<O[key]> };
+/**@type Validator */
 export type Validator<O extends { [P in keyof O]: O[P] }> = { [key in keyof O]?: Joi.Schema };
+/**@type SnapshotVersion */
 export type SnapshotVersion<O extends { [P in keyof O]: O[P] }> = { meta: { timestamp: Date }; value: Snapshot<O> };
 
+/**@interface EventTypes */
 export interface EventTypes<O> {
   'ghii:version:first': undefined;
   'ghii:version:new': SnapshotVersion<O>;
 }
+/**@interface GhiiEmitter */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface GhiiEmitter<O> extends TypedEventEmitter<EventTypes<O>> {}
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+
+/**
+ * @description Instance of Ghii object
+ * @type {O extends { [P in keyof O]: O[P] }}
+ * @returns {GhiiInstance}
+ */
 export function ghii<O extends { [P in keyof O]: O[P] }>(): GhiiInstance<O> {
   type ObjectKeys = keyof O;
 
@@ -46,12 +65,24 @@ export function ghii<O extends { [P in keyof O]: O[P] }>(): GhiiInstance<O> {
   const versions: SnapshotVersion<O>[] = [];
 
   const events = (new EventEmitter() as unknown) as GhiiEmitter<O>;
+  /**
+   * Create a section and return the updated object
+   * @property  {GhiiInstance<O>} section
+   * @param {GhiiInstance<O>} this
+   * @param {K} name
+   * @param {Topic<O[K]>} topic
+   */
   function section<K extends ObjectKeys>(this: GhiiInstance<O>, name: K, topic: Topic<O[K]>): ReturnType<typeof ghii> {
     sections[name] = topic;
     validators[name] = topic.validator(Joi);
     return this;
   }
 
+  /** Create a loader and return the updated object
+   * @property {GhiiInstance<O>}loader
+   * @param {GhiiInstance<O>} this
+   * @param {Loader} loader
+   */
   function loader(this: GhiiInstance<O>, loader: Loader) {
     loaders.push(loader);
     return this;
@@ -98,6 +129,7 @@ export function ghii<O extends { [P in keyof O]: O[P] }>(): GhiiInstance<O> {
     return validation.filter(promise => promise.status === 'rejected');
   }
 
+  /** @property {Promise<{ [key in keyof O]: O[key] }>} takeSnapshot */
   async function takeSnapshot(): Promise<Snapshot<O>> {
     const defaults = prepareDefaults(sections);
 
@@ -110,6 +142,9 @@ export function ghii<O extends { [P in keyof O]: O[P] }>(): GhiiInstance<O> {
     return result;
   }
 
+  /** returns a deep copy of snapshot versions
+   * @property {SnapShotVersion<O>[]} history
+   */
   function history() {
     return cloneDeep(versions);
   }
@@ -119,6 +154,10 @@ export function ghii<O extends { [P in keyof O]: O[P] }>(): GhiiInstance<O> {
     return cloneDeep(versions[versions.length - 1]);
   }
 
+  /**
+   * returns latest version or the default
+   * @property {O} snapshot
+   * @param {Snapshot<O>} snapshot */
   function snapshot(newSnapshot?: Snapshot<O>) {
     if (newSnapshot) {
       versions.push({ meta: { timestamp: new Date() }, value: newSnapshot });
@@ -129,6 +168,9 @@ export function ghii<O extends { [P in keyof O]: O[P] }>(): GhiiInstance<O> {
     return latestVersion()?.value ?? prepareDefaults(sections);
   }
 
+  /** wait for the first snapshot
+   * @property {SnapShotVersion | undefined} latestVersion
+   */
   function waitForFirstSnapshot(options?: { timeout?: number; onTimeout?: () => void }, ...moduleToLoad: string[]) {
     const { timeout = 30000, onTimeout } = options || {};
 
